092711e12dbe5c8cc0e40c917d09bd913d64ebbe
Pedr0-Ricard <pedrorricardo17@gmail.com>
2025-06-02 11:08:08 -0300

    ensure LoginRouter returns 400 if an invalid email is provided

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,4 +1,4 @@
-const MissingParamError = require('./missing-param-error')
+const MissingParamError = require('./invalid-param-error')
 const UnauthorizedError = require('./unauthorized-error')
 const ServerError = require('./server-error')
 module.exports = class HttpResponse {
