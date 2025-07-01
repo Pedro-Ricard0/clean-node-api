c60a32ffce88077d13c2f5ab0923430295f00a22
Pedr0-Ricard <pedrorricardo17@gmail.com>
2025-07-01 14:45:44 -0300

    move test CORS to its own file

--- main/config/setup.test.js
+++ main/config/setup.test.js
@@ -8,14 +8,4 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
-
-  test('Should enable CORS', async () => {
-    app.get('/test_cors', (req, res) => {
-      res.send('')
-    })
-    const res = await request(app).get('/test_cors')
-    expect(res.headers['acess-control-allow-origin']).toBe('*')
-    expect(res.headers['acess-control-allow-methods']).toBe('*')
-    expect(res.headers['acess-control-allow-headers']).toBe('*')
-  })
 })
