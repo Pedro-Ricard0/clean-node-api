f08f34727571b9a06068018fc25662ed57e57ab0
Pedr0-Ricard <pedrorricardo17@gmail.com>
2025-07-01 14:55:02 -0300

    ensure requests parse body as JSON

--- main/config/setup.js
+++ main/config/setup.js
@@ -1,5 +1,7 @@
+const jsonParser = require('../middlewares/json-parser.js')
 const cors = require('../middlewares/cors.js')
 module.exports = (app) => {
   app.disable('x-powered-by')
   app.use(cors)
+  app.use(jsonParser)
 }
---
+++ main/middlewares/json-parser.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../config/app.js')
+describe('JSON Parser Middleware', () => {
+  test('Should parse body as JSON', async () => {
+    app.post('/test_json_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_json_parser')
+      .send({ name: 'Mango' })
+      .expect({ name: 'Mango' })
+  })
+})
