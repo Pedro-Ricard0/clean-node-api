b4d1daa8c7ebc7bea9f4bac6f3d0b979cc98aaae
Pedr0-Ricard <pedrorricardo17@gmail.com>
2025-07-01 15:24:00 -0300

    ensure requests return given content-type when forced

--- main/middlewares/content-type.test.js
+++ main/middlewares/content-type.test.js
@@ -1,12 +1,32 @@
 const request = require('supertest')
-const app = require('../config/app.js')
+const express = require('express')
+const setupApp = require('../config/setup')
+
 describe('Content-Type Middleware', () => {
   test('Should return json content-type as default', async () => {
-    app.get('/test_content-type', (req, res) => {
+    const app = express()
+    setupApp(app)
+
+    app.get('/test_content_type', (req, res) => {
       res.send({})
     })
+
     await request(app)
-      .get('/test_content-type')
+      .get('/test_content_type')
       .expect('content-type', /json/)
+  })
+
+  test('Should return xml content-type if forced', async () => {
+    const app = express()
+    setupApp(app)
+
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('<note><to>Pedro</to></note>')
+    })
+
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
   })
 })
